04e1f9f32f9586a1c4f73f8d320511550e49fc7f
Pia <pia.fichtl@uni-weimar.de>
2018-06-08 16:17:17 +0200

    made android project

---
+++ seismo/app/src/main/java/com/example/piafichtl/seismo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.piafichtl.seismo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
